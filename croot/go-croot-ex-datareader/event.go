c3032893d053106b6489b6576ac69b765f41ddb2
Sebastien Binet <binet@cern.ch>
2013-06-06 17:42:07 +0200

    add croot datareader example

---
+++ croot/go-croot-ex-datareader/event.go
@@ -0,0 +1,162 @@
+// automatically generated!
+
+package main
+
+import (
+  "fmt"
+
+  "github.com/go-hep/croot"
+)
+
+
+type NTExtraVars struct {
+	Nvtx uint32
+	Metjet float32
+	Metsjet float32
+	Metcell float32
+	Metel float32
+	Metmu float32
+	Metph float32
+	Mettrack float32
+	Mettrack_phi float32
+	MT2 float32
+	Thrust float32
+	Thrust_axis_phi float32
+	Sphericity float32
+	MCT float32
+	NISRJet int32
+	IsrjetPt float32
+	MT2_noISR float32
+	Thrust_noISR float32
+	Thrust_axis_phi_noISR float32
+	Sphericity_noISR float32
+	MCT_noISR float32
+}
+
+type NTVars struct {
+	RunNumber uint32
+	EventNumber uint32
+	Veto uint32
+	EventWeight float32
+	PileupWeight float32
+	PileupWeightUp float32
+	PileupWeightDown float32
+	GenWeight float32
+	NJet uint32
+	Jet1Pt float32
+	Jet2Pt float32
+	Jet3Pt float32
+	Jet4Pt float32
+	Jet5Pt float32
+	Jet6Pt float32
+	Jet1Eta float32
+	Jet2Eta float32
+	Jet3Eta float32
+	Jet4Eta float32
+	Jet5Eta float32
+	Jet6Eta float32
+	Jet1Phi float32
+	Jet2Phi float32
+	Jet3Phi float32
+	Jet4Phi float32
+	Jet5Phi float32
+	Jet6Phi float32
+	Jet1M float32
+	Jet2M float32
+	Jet3M float32
+	Jet4M float32
+	Jet5M float32
+	Jet6M float32
+	Met float32
+	MetPhi float32
+	DPhi float32
+	DPhiR float32
+	Meff1Jet float32
+	Meff2Jet float32
+	Meff3Jet float32
+	Meff4Jet float32
+	Meff5Jet float32
+	Meff6Jet float32
+	MeffInc float32
+	Hardproc int32
+	NBJet uint32
+	BTagWeight float32
+	BTagWeightBUp float32
+	BTagWeightBDown float32
+	BTagWeightCUp float32
+	BTagWeightCDown float32
+	BTagWeightLUp float32
+	BTagWeightLDown float32
+	NormWeight float32
+	NormWeightUp float32
+	NormWeightDown float32
+	Cleaning uint32
+	Timing float32
+	Jet1Emf float32
+	Jet2Emf float32
+	Jet1Chf float32
+	Jet2Chf float32
+	PdfId1 int32
+	PdfId2 int32
+	TauN uint32
+	TauJetBDTLoose uint32
+	TauMt float32
+}
+
+
+
+type DataReader struct {
+	NTExtraVars NTExtraVars
+	NTVars NTVars
+
+
+ // branches
+	b_NTExtraVars croot.Branch
+	b_NTVars croot.Branch
+
+
+	Tree croot.Tree
+}
+
+func NewDataReader(tree croot.Tree) (*DataReader, error) {
+	dr := &DataReader{}
+	err := dr.Init(tree)
+	if err != nil {
+		return nil, err
+	}
+	return dr, nil
+}
+
+func (dr *DataReader) Init(tree croot.Tree) error {
+	var err error
+	var o int32
+	dr.Tree = tree
+
+	o = dr.Tree.SetBranchAddress("NTExtraVars", &dr.NTExtraVars)
+	if o < 0 {
+		return fmt.Errorf("invalid branch: [NTExtraVars] (got %d)", o)
+	}
+
+	o = dr.Tree.SetBranchAddress("NTVars", &dr.NTVars)
+	if o < 0 {
+		return fmt.Errorf("invalid branch: [NTVars] (got %d)", o)
+	}
+
+	return err
+}
+
+func (dr *DataReader) GetEntry(entry int64) int {
+	if dr.Tree == nil {
+		return 0
+	}
+	return dr.Tree.GetEntry(entry, 1)
+}
+
+
+
+func init() {
+	// register all generated types with CRoot
+	croot.RegisterType(&NTExtraVars{})
+	croot.RegisterType(&NTVars{})
+
+}
---
+++ croot/go-croot-ex-datareader/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/go-hep/croot"
+)
+
+var (
+	fname = flag.String("file", "", "path to a ROOT file to read")
+	tname = flag.String("tree", "", "name of the TTree to read")
+)
+
+func main() {
+	flag.Parse()
+
+	if *fname == "" {
+		fmt.Fprintf(os.Stderr, "**error** you need to give a ROOT file to read")
+		os.Exit(1)
+	}
+
+	if *tname == "" {
+		fmt.Fprintf(os.Stderr, "**error** you need to give a TTree name to read")
+		os.Exit(1)
+	}
+
+	fmt.Printf(":: opening file [%s]...\n", *fname)
+	f, err := croot.OpenFile(*fname, "read", "my event file", 1, 0)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "**error** %v\n", err)
+		os.Exit(1)
+	}
+
+	defer f.Close("")
+
+	t := f.GetTree(*tname)
+	evt, err := NewDataReader(t)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "**error** %v\n", err)
+		os.Exit(1)
+	}
+
+	evtmax := evt.Tree.GetEntries()
+	fmt.Printf(":: entries: %v\n", evtmax)
+	for iev := int64(0); iev != evtmax; iev++ {
+		do_display := iev%(evtmax/10) == 0
+		if do_display {
+			fmt.Printf(":: processing event %d...\n", iev)
+		}
+		if evt.GetEntry(iev) <= 0 {
+			fmt.Fprintf(os.Stderr, "**error** processing event %d\n", iev)
+			os.Exit(1)
+		}
+		if do_display {
+			fmt.Printf("evt: %v %v\n", evt.NTVars.RunNumber, evt.NTVars.EventNumber)
+		}
+	}
+}
