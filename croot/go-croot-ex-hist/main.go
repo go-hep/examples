6212883eb1bc528802973c42d91b126d77f35daa
Sebastien Binet <binet@cern.ch>
2013-06-07 09:18:21 +0200

    croot-hist: also compare with go-hep/random

--- croot/go-croot-ex-hist/main.go
+++ croot/go-croot-ex-hist/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/go-hep/croot"
+	"github.com/go-hep/random"
 )
 
 var (
@@ -28,20 +29,28 @@ func main() {
 	}
 	defer f.Close("")
 
-	h1 := croot.NewH1F("h1", "h1", 100, -10, 10)
+	h1 := croot.NewH1F("h1", "h1 - math/rand", 100, -10, 10)
 	if h1 == nil {
 		fmt.Fprintf(os.Stderr, "**error** creating histogram [h1]\n")
 		os.Exit(1)
 	}
 
-	h2 := croot.NewH1F("h2", "h2", 100, -10, 10)
+	h2 := croot.NewH1F("h2", "h2 - croot.GRandom", 100, -10, 10)
 	if h2 == nil {
 		fmt.Fprintf(os.Stderr, "**error** creating histogram [h2]\n")
 		os.Exit(1)
 	}
 
+	h3 := croot.NewH1F("h3", "h3 - go-hep/random", 100, -10, 10)
+	if h3 == nil {
+		fmt.Fprintf(os.Stderr, "**error** creating histogram [h3]\n")
+		os.Exit(1)
+	}
+
 	stddev := 5.0
 	mean := 0.0
+
+	gauss := random.Gauss(mean, stddev, nil)
 
 	for i := 0; i < *nentries; i++ {
 		do_display := i%(*nentries/10) == 0
@@ -50,13 +59,15 @@ func main() {
 		}
 		x_1 := rand.NormFloat64()*stddev + mean
 		x_2 := croot.GRandom.Gaus(mean, stddev)
+		x_3 := gauss()
 
 		h1.Fill(x_1, 1)
 		h2.Fill(x_2, 1)
+		h3.Fill(x_3, 1)
 	}
 
-	for _, h := range []croot.H1F{h1, h2} {
-		fmt.Printf(":: %s ===============\n", h.GetTitle())
+	for _, h := range []croot.H1F{h1, h2, h3} {
+		fmt.Printf("===== %-15s =====\n", h.GetTitle())
 		fmt.Printf("   Entries= %8.3f\n", h.GetEntries())
 		fmt.Printf("   Mean=    %8.3f\n", h.GetMean())
 		fmt.Printf("   RMS=     %8.3f\n", h.GetRMS())
