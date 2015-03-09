a4fe11f27e8163546760511dfe08cecc587d5249
Sebastien Binet <binet@cern.ch>
2015-03-09 22:03:42 +0100

    hbook: add r/w example for H1D histos

---
+++ hbook/hbook-r-hsimple/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/go-hep/hbook"
+	"github.com/go-hep/rio"
+)
+
+var (
+	fname = flag.String("i", "hsimple.rio", "input file name")
+)
+
+func main() {
+	flag.Parse()
+
+	fmt.Printf(":: opening input file [%s]...\n", *fname)
+	f, err := os.Open(*fname)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	r, err := rio.NewReader(f)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	fmt.Printf(":: loading histos from [%s]...\n", *fname)
+	var (
+		h1 hbook.H1D
+		h2 hbook.H1D
+	)
+
+	err = read(r, "h1", &h1)
+	if err != nil {
+		panic(err)
+	}
+
+	err = read(r, "h2", &h2)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s: entries=%v mean=%+8.3f RMS=%+8.3f\n",
+		h1.Name(), h1.Entries(), h1.Mean(), h1.RMS(),
+	)
+	fmt.Printf("%s: entries=%v mean=%+8.3f RMS=%+8.3f\n",
+		h2.Name(), h2.Entries(), h2.Mean(), h2.RMS(),
+	)
+
+	err = r.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func read(r *rio.Reader, name string, ptr interface{}) error {
+	var err error
+
+	rec := r.Record(name)
+	err = rec.Connect(name, ptr)
+	if err != nil {
+		return err
+	}
+
+	err = rec.Read()
+	if err != nil {
+		return err
+	}
+
+	blk := rec.Block(name)
+	if blk == nil {
+		return fmt.Errorf("no block [%s] in record [%s]", name, name)
+	}
+
+	err = blk.Read(ptr)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
---
+++ hbook/hbook-w-hsimple/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"compress/flate"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+
+	"github.com/go-hep/hbook"
+	"github.com/go-hep/rio"
+)
+
+var (
+	fname = flag.String("o", "hsimple.rio", "output file name")
+)
+
+func main() {
+	flag.Parse()
+
+	fmt.Printf(":: creating output file [%s]...\n", *fname)
+	f, err := os.Create(*fname)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	w, err := rio.NewWriter(f)
+	if err != nil {
+		panic(err)
+	}
+	defer w.Close()
+
+	err = w.SetCompressor(rio.CompressZlib, flate.DefaultCompression)
+	if err != nil {
+		panic(err)
+	}
+
+	h1 := hbook.NewH1D(100, -10., 10.)
+	if h1 == nil {
+		panic(fmt.Errorf("nil pointer to H1D"))
+	}
+
+	h1.Annotation()["name"] = "h1"
+
+	h2 := hbook.NewH1D(100, -10., 10.)
+	if h2 == nil {
+		panic(fmt.Errorf("nil pointer to H1D"))
+	}
+
+	h2.Annotation()["name"] = "h2"
+
+	// draw random values from a gaussian distribution
+	const n = 1000
+	fmt.Printf(":: filling histos with %d events...\n", n)
+	rand.Seed(0)
+	for i := 0; i < n; i++ {
+		v1 := rand.NormFloat64()
+		h1.Fill(v1, 1.0)
+
+		v2 := rand.NormFloat64()
+		h2.Fill(v2+2, 1.0)
+	}
+	fmt.Printf("%s: entries=%v mean=%+8.3f RMS=%+8.3f\n",
+		h1.Name(), h1.Entries(), h1.Mean(), h1.RMS(),
+	)
+	fmt.Printf("%s: entries=%v mean=%+8.3f RMS=%+8.3f\n",
+		h2.Name(), h2.Entries(), h2.Mean(), h2.RMS(),
+	)
+
+	fmt.Printf(":: saving histos to [%s]...\n", *fname)
+	err = write(w, "h1", h1)
+	if err != nil {
+		panic(err)
+	}
+
+	err = write(w, "h2", h2)
+	if err != nil {
+		panic(err)
+	}
+
+	err = w.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func write(w *rio.Writer, name string, obj interface{}) error {
+	var err error
+
+	rec := w.Record(name)
+	err = rec.Connect(name, obj)
+	if err != nil {
+		return err
+	}
+
+	blk := rec.Block(name)
+	err = blk.Write(obj)
+	if err != nil {
+		return err
+	}
+
+	err = rec.Write()
+	if err != nil {
+		return err
+	}
+
+	return err
+}
